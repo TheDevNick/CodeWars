ceed24788576baea08adf6dd4e72d7f2a4c27bf5
Nick Hollins <nholl22@wgu.edu>
2022-05-15 18:59:53 -0500

    Coding challenge for 5-15-22

---
+++ Strings/js/boolToStr.js
@@ -0,0 +1,17 @@
+/* 
+Implement a function which convert the given boolean value into its string representation.
+
+Note: Only valid inputs will be given.
+
+P: function takes in a boolean
+R: function returns string 'true' if the boolean is true and 'false' if the boolean is false
+E: booleanToString(true) should return 'true'
+P: a if statement (or ternary operator) to see if value is true or false
+*/
+
+
+function booleanToString(b){
+    return b === true ? 'true' : 'false'
+  }
+
+  
