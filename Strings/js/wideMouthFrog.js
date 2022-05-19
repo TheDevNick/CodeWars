2ac1c5d075c6fa634cd54ad13f502cb3d4b10131
Nick Hollins <nholl22@wgu.edu>
2022-05-18 20:26:43 -0500

    updated coding challenge

--- Strings/js/wideMouthFrog.js
+++ Strings/js/wideMouthFrog.js
@@ -7,6 +7,19 @@ When he meets the alligator, it then makes a tiny mouth.
 
 Your goal in this kata is to create complete the mouth_size method this method takes one argument animal which corresponds to the animal encountered by the frog. If this one is an alligator (case-insensitive) return small otherwise return wide
 
+P: function takes one arg which is animal 
+R: if alligator, return small , if not , return wide
+E: mouthSize(alligator) => small
+    mouthSize(rabbit) => wide
+P: need a conditional (if statement)
+    -if animal is alligator (case-insensitive) , return small, else return wide 
 
 
 */
+
+function mouthSize(animal) {
+    // code here
+    return animal === 'alligator' || animal === 'ALLIGATOR' ? 'small' : 'wide'
+  }
+
+  console.log(mouthSize('alligator'))
