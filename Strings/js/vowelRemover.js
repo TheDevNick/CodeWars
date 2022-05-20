04a93f46012de80549062e0922d4725a5f547a63
Nick Hollins <nholl22@wgu.edu>
2022-05-19 20:50:42 -0500

    coding challenge for 5-19-2022

---
+++ Strings/js/vowelRemover.js
@@ -0,0 +1,22 @@
+/* 
+Create a function called shortcut to remove the lowercase vowels (a, e, i, o, u ) in a given string.
+
+Examples
+"hello"     -->  "hll"
+"codewars"  -->  "cdwrs"
+"goodbye"   -->  "gdby"
+"HELLO"     -->  "HELLO"
+don't worry about uppercase vowels
+y is not considered a vowel for this kata
+
+P: function will take a string
+R: string will be returned with no lowercase vowels (a, e, i, o, u )
+E: "goodbye"   -->  "gdby"
+P: - find out if any vowels exist
+- find the index of that vowel in the string and remove it
+
+*/
+
+function shortcut (string) {
+    return '';
+  }
