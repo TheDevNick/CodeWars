6afebc787e3b0bc53322aaa6d7684d0f86a84ab8
Nick Hollins <nholl22@wgu.edu>
2022-05-27 19:04:14 -0500

    coding challenge for 5-27-22

--- Strings/js/alan.js
+++ Strings/js/alan.js
@@ -16,4 +16,4 @@ function apple(x){
     return num**2 > 1000 ? 'It\'s hotter than the sun!!' : 'Help yourself to a honeycomb Yorkie for the glovebox'
 }
 
-console.log(apple(50))
+console.log(apple(4))
