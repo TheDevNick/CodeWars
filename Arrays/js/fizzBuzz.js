13f95b0efa721ea6f057a927466d775c472f1439
Nick Hollins <nholl22@wgu.edu>
2022-06-12 10:55:07 -0500

    coding challenge for 6-12-22

---
+++ Arrays/js/fizzBuzz.js
@@ -0,0 +1,32 @@
+/* 
+Return an array containing the numbers from 1 to N, where N is the parametered value.
+
+Replace certain values however if any of the following conditions are met:
+
+If the value is a multiple of 3: use the value "Fizz" instead
+If the value is a multiple of 5: use the value "Buzz" instead
+If the value is a multiple of 3 & 5: use the value "FizzBuzz" instead
+N will never be less than 1.
+
+Method calling example:
+
+fizzbuzz(3) -->  [1, 2, "Fizz"]
+
+P: function takes in a number > 1
+R: returns an array
+E: fizzbuzz(3) -->  [1, 2, "Fizz"]
+P: - use a  loop to loop from 1 to N
+    - do a check to see if the number is a multiple of 3 or 5
+*/
+
+// Return an array
+function fizzbuzz(n)
+{
+  //
+  let arr = []
+  for (let i = 0; i <= n; i++) {
+  
+  }
+}
+
+console.log(fizzbuzz(3))
