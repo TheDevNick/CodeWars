e33a149159ac45b255ddaa782dc63df49f2c92b0
Nick Hollins <nholl22@wgu.edu>
2022-07-12 07:02:48 -0500

    coding challenge for 7-12-22

---
+++ Arrays/js/codingMeetup1.js
@@ -0,0 +1,36 @@
+/* 
+You will be given an array of objects (hashes in ruby) representing data about developers who have signed up to attend the coding meetup that you are organising for the first time.
+
+Your task is to return the number of JavaScript developers coming from Europe.
+For example, given the following list:
+
+var list1 = [
+  { firstName: 'Noah', lastName: 'M.', country: 'Switzerland', continent: 'Europe', age: 19, language: 'JavaScript' },
+  { firstName: 'Maia', lastName: 'S.', country: 'Tahiti', continent: 'Oceania', age: 28, language: 'JavaScript' },
+  { firstName: 'Shufen', lastName: 'L.', country: 'Taiwan', continent: 'Asia', age: 35, language: 'HTML' },
+  { firstName: 'Sumayah', lastName: 'M.', country: 'Tajikistan', continent: 'Asia', age: 30, language: 'CSS' }
+];
+your function should return number 1.
+
+P: function takes in array of objects
+R: function returns number of devs from Europe
+E: see above
+P: 
+    - filter and get all devs from Europe and language is js
+    - get a count of devs from Europe and language is js
+    - return that count
+*/
+
+function countDevelopers(list) {
+    // your awesome code here :)
+    return list.filter(con => con.continent === 'Europe' && con.language === 'JavaScript').length
+  }
+
+  var list1 = [
+    { firstName: 'Noah', lastName: 'M.', country: 'Switzerland', continent: 'Europe', age: 19, language: 'JavaScript' },
+    { firstName: 'Noah', lastName: 'M.', country: 'Switzerland', continent: 'Europe', age: 19, language: 'JavaScript' },
+    { firstName: 'Maia', lastName: 'S.', country: 'Tahiti', continent: 'Oceania', age: 28, language: 'JavaScript' },
+    { firstName: 'Shufen', lastName: 'L.', country: 'Taiwan', continent: 'Asia', age: 35, language: 'HTML' },
+    { firstName: 'Sumayah', lastName: 'M.', country: 'Tajikistan', continent: 'Asia', age: 30, language: 'CSS' }
+  ];
+  console.log(countDevelopers(list1))
