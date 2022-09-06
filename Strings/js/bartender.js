81801f8314e83d8a13152ec8c70da3074f605f7e
Nick Hollins <nholl22@wgu.edu>
2022-09-05 19:11:59 -0500

    coding challenge for 9-5-22

---
+++ Strings/js/bartender.js
@@ -0,0 +1,38 @@
+/* 
+Complete the function that receives as input a string, and produces outputs according to the following table:
+
+Input	Output
+"Jabroni"	"Patron Tequila"
+"School Counselor"	"Anything with Alcohol"
+"Programmer"	"Hipster Craft Beer"
+"Bike Gang Member"	"Moonshine"
+"Politician"	"Your tax dollars"
+"Rapper"	"Cristal"
+anything else	"Beer"
+Note: anything else is the default case: if the input to the function is not any of the values in the table, then the return value should be "Beer".
+
+Make sure you cover the cases where certain words do not show up with correct capitalization. For example, the input "pOLitiCIaN" should still return "Your tax dollars".
+
+function takes in a string 
+function returns a string 
+
+
+
+*/
+// create an object to hold values
+const drinks = {
+  "jabroni": "Patron Tequila",
+  "school counselor": "Anything with Alcohol",
+  "programmer": "Hipster Craft Beer",
+  "bike gang member": "Moonshine",
+  "politician": "Your tax dollars",
+  "rapper": "Cristal",
+}
+
+function getDrinkByProfession(param) {
+    // access the drink object's param using the brackets and return beer if it's not found
+    return drinks[param.toLowerCase()] || "beer"
+
+}
+
+console.log(getDrinkByProfession("Jabroni"), "Patron Tequila")
