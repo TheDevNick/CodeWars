fdde35ad99f90e6f5b7f1ba7d0af2ff98592432f
Nick Hollins <nholl22@wgu.edu>
2022-09-14 21:31:19 -0500

    turorial purposes

---
+++ Arrays/js/test.js
@@ -0,0 +1,22 @@
+/* 
+array methods
+*/
+
+// splice
+let arr1 = ['I', 'love', 'js']
+const js = arr1.splice(2, 1)
+const love = arr1.splice(1, 1)
+// console.log(arr1)
+// console.log(js)
+// console.log(love)
+
+// slice
+let arr2 = ['n', 'i', 'c', 'k']
+// console.log(arr2.slice(0,1))
+
+// concat
+let arr3 = [1,2,3]
+// add 4, 5
+console.log(arr3.concat(4,5))
+
+// tutorial
