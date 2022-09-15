4481359d3299cee8108bc5004ce32ea70c3e4f15
Nick Hollins <nholl22@wgu.edu>
2022-09-15 16:07:25 -0500

    adding problems for future tutorial

--- Arrays/js/test.js
+++ Arrays/js/test.js
@@ -17,6 +17,44 @@ let arr2 = ['n', 'i', 'c', 'k']
 // concat
 let arr3 = [1,2,3]
 // add 4, 5
-console.log(arr3.concat(4,5))
+// console.log(arr3.concat(4,5))
 
 // tutorial
+
+let arr4 = ['nick', 'hollin', 'aquile']
+
+arr4.forEach(name => console.log(name))
+
+
+let users = [
+  { id: 1, name: "John" },
+  { id: 1, name: "John" },
+  { id: 2, name: "Pete" },
+  { id: 3, name: "Mary" },
+]
+
+
+const john = users.filter((user) => user.id === 1)
+const usersUnder5 = users.filter((user) => user.name.startsWith('p'.toUpperCase()))
+// console.log(john)
+// console.log(usersUnder5)
+
+// map
+let workers = ['Nick', 'Lex', 'Lani', 'Jon']
+
+const workersNameLengths = workers.map(item => item.length)
+console.log(workersNameLengths)
+
+let newPeople = 'jake, john, james'
+let myArrr = newPeople.split(', ')
+for (const name of myArrr) console.log(name)
+
+const testArr = ['bulls', 'wizards', 'nets']
+
+const newStr = testArr.join('!')
+console.log(newStr)
+
+// reduce
+let ages = [1, 2, 5, 10]
+let ageSum =  ages.reduce((acc, curr) => acc + curr,0)
+console.log(ageSum)
