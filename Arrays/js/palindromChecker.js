fd6a98993d1cdc0c08285e08981d5b2ace4814bd
Nick Hollins <nholl22@wgu.edu>
2022-10-17 15:43:25 -0500

    coding challenge 10-17-22

---
+++ Arrays/js/palindromChecker.js
@@ -0,0 +1,25 @@
+/* CHALLENGE
+Given a string of text, return true or false indicating whether or not the text is a palindrome.
+e.g palindromeChecker('racecar') // will return true
+
+-- palidromeChecker -- string of text -- always be letters (no special chars ) -- may have uppercase letters -- 
+-- returning true or false
+-- 
+*/
+
+function palindromeChecker(text) {
+  // Code goes here
+  const reversedText = text.split("").reverse().join("").toLowerCase()
+  const isPalindrome = text.toLowerCase() === reversedText
+  // turn text into array
+  // create variable to store the word in reverse
+  // compare original text vs reversed text
+  // return true if they match , false if not
+  return isPalindrome
+}
+
+console.log(palindromeChecker("racecar"), true)
+console.log(palindromeChecker("bob"), true)
+console.log(palindromeChecker("wow"), true)
+console.log(palindromeChecker("php"), true)
+console.log(palindromeChecker("2002"), true)
