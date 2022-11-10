9beb4706500beb664fcacb0c83f8625b65883667
Nick Hollins <nholl22@wgu.edu>
2022-11-10 14:42:27 -0600

    I made a mistake, this is O(N) time complexity not O(1)

--- Arrays/js/lostNumber.js
+++ Arrays/js/lostNumber.js
@@ -31,3 +31,4 @@ const findDeletedNumber = (arr, mixedArr) => {
 console.log(findDeletedNumber([1,2,3,4], [3,4,1]), 2)
 console.log(findDeletedNumber([1,2,3,4], [3,4,1,2]), 0)
 console.log(findDeletedNumber([1,2,3,4,5,6,7,8,9], [1,9,7,4,6,2,3,8]), 5)
+// Time complexity: O(N)
